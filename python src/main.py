391534ecf85d9f37a43b9d781c65a1b34e13c00f
Sneha sab <snehasahit@gmail.com>
2026-01-13 12:26:17 +0530

    Create main.py

---
+++ python src/main.py
@@ -0,0 +1,11 @@
+
+## 👤 Author
+**Sneha sah**  
+B.Tech 2nd Year, University of Calcutta  
+GitHub: https://github.com/snehasah1 
+LinkedIn: https://www.linkedin.com/in/sneha-s-6774a03a5/
+
+## ⭐ Future Improvements
+- Add more features
+- Improve UI
+- Deploy the project
